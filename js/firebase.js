2f74fe7dd1b2b17e1d85f5018ebc6599d1fb60c4
Shrinivas Biradar <biradarshri810@gmail.com>
2025-12-13 22:46:59 +0530

    Updated habit tracker changes

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,24 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// --- CORRECTED Contents of js/firebase.js ---
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAxCpcCNp8o5MGwV6S3nMH7rI9f2gO2V7I",
-  authDomain: "streakforge-01.firebaseapp.com",
-  projectId: "streakforge-01",
-  storageBucket: "streakforge-01.firebasestorage.app",
-  messagingSenderId: "126111347012",
-  appId: "1:126111347012:web:2189aa07b7ac767da8a711",
-  measurementId: "G-65MHCLYPNE"
+    apiKey: "AIzaSyAxCpcCNp8o5MGwV6S3nMH7rI9f2gO2V7I",
+    authDomain: "streakforge-01.firebaseapp.com",
+    projectId: "streakforge-01",
+    storageBucket: "streakforge-01.firebasestorage.app",
+    messagingSenderId: "126111347012",
+    appId: "1:126111347012:web:2189aa07b7ac767da8a711",
+    measurementId: "G-65MHCLYPNE"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// 1. Initialize Firebase App (using the v8-style global 'firebase' object)
+firebase.initializeApp(firebaseConfig);
+
+// 2. Define the global 'auth' and 'db' variables that js/auth.js is looking for
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+// Note: You can remove the 'analytics' part as it's not needed for login/signup
+// and would require its own import from the modular syntax.
+
+// --- END of CORRECTED js/firebase.js ---
